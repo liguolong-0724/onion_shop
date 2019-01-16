6348c06a65a070149bb7981a6262c5a647b02e5c
liguolong-0724 <286932995@qq.com>
2019-01-16 18:24:17 +0800

    project init commit

---
+++ src/main/java/com/onion/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.onion.dao;
+
+import com.onion.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
